27c1559c945ddd7ebbfd30a31294c55a902fc98c
ThomasDubernet <thoms.dubernet@gmail.com>
2022-10-06 20:03:48 +0200

    first commit

---
+++ src/faker.ts
@@ -0,0 +1,8 @@
+export const faker = () => {
+  return [
+    { firstname: "Thomas", lastname: "Dubernet" },
+    { firstname: "Mike", lastname: "Growming" },
+    { firstname: "Flo", lastname: "Lazarus" },
+    { firstname: "John", lastname: "Doe" },
+  ];
+};
